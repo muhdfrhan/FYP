322c431fe6bf83a547503479b111e73639fcbfb6
muhd farhan <120719673+muhdfrhan@users.noreply.github.com>
2025-06-18 02:01:22 +0800

    Update navbar title to "Zakat Supervisor Portal"

--- src/components/Navbars/AdminNavbar.js
+++ src/components/Navbars/AdminNavbar.js
@@ -8,7 +8,7 @@ const AdminNavbar = () => {
   return (
    <Navbar style={{ backgroundColor: "#000000" }} className="navbar-top navbar-dark" expand="md" id="navbar-main">
       <Container fluid>
-        <h4 className="mb-0 text-white text-uppercase d-none d-lg-inline-block">Admin Portal</h4>
+        <h4 className="mb-0 text-white text-uppercase d-none d-lg-inline-block">Zakat Supervisor Portal</h4>
         <span className="mb-0 font-weight-bold text-white">
           Welcome, {staffName}
         </span>
